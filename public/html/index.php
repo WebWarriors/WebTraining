f831ce8ddd52c3837443e3b5045e60249d2f5a73
a.panapadeatchy <a.panapadeatchy@rt-iut.re>
2015-05-20 21:10:33 +0400

    First tests
    MaterializeCss
    Structure Index.php

---
+++ public/html/index.php
@@ -0,0 +1,64 @@
+<?php include("partials/header.php"); ?>
+    <div class="container">
+            <h1 class="header center blue-text">Welcome</h1>
+        <div class="row">
+            <div class="col s12">
+                <p class="center">
+                    Description. This is a super website ! <br/><br/>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nam laoreet egestas libero,
+                    vitae mollis massa hendrerit sed. Fusce in arcu libero. Maecenas posuere cursus ex,
+                    non blandit augue luctus a. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+                    Vestibulum egestas enim at mauris aliquet varius. Sed elit arcu, vehicula et venenatis
+                    sagittis, porttitor vel sapien. Cras egestas, lectus sit amet malesuada fermentum, nunc
+                    diam euismod dui, in faucibus nulla purus a enim.<br/>
+                    Aliquam vestibulum, sem ac consequat commodo, turpis quam commodo ante, eget gravida
+                    ante arcu ut tortor. Mauris in gravida ipsum, vitae tristique felis. Donec semper risus
+                    ac auctor cursus. Etiam tincidunt sit amet nisi eget pulvinar. Aliquam tempus eu sapien
+                    ut dictum. Praesent dui nunc, sollicitudin vel velit at, fringilla mattis tellus.
+                    Vestibulum metus nisi, vestibulum nec leo quis, vehicula porta urna. Sed in egestas eros.
+                    Fusce ultricies ut lacus sit amet pretium.
+                </p>
+            </div>
+        </div>
+    </div> <!-- /.container -->    
+    <div class="container">
+        <div class="section">
+            <div class="row">
+                <div class="col s4">
+                    <div class="icon-block">
+                        <h5 class="center"><a href="#">See example</a></h5>
+                        <p class="light center-align">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia inventore nemo dolores consequatur optio pariatur cum distinctio, recusandae nam dolorum?</p>
+                    </div>
+                </div>
+                <div class="col s4">
+                    <div class="icon-block">
+                        <h5 class="center"><a href="#">Create account</a></h5>
+                        <p class="light">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia inventore nemo dolores consequatur optio pariatur cum distinctio, recusandae nam dolorum?</p>
+                    </div>                
+                </div>
+                <div class="col s4">
+                    <div class="icon-block">
+                        <h5 class="center"><a href="#">Map</a></h5>
+                        <p class="light">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Officia inventore nemo dolores consequatur optio pariatur cum distinctio, recusandae nam dolorum?</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <footer class="page-footer blue">
+        <div class="container">
+           <div class="col s4">
+                <p class="white-text">
+                    About : Why this website ?<br>
+                    Who?<br><br> 
+                </p>
+           </div>
+        </div>
+        <div class="footer-copyright">
+            <div class="container">
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+            </div>
+        </div>
+    </footer>
+<?php include("partials/footer.php"); ?>
---
+++ public/html/partials/header.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html>
+	<head>
+	  	<!--Import materialize.css-->
+	  	<link type="text/css" rel="stylesheet" href="../css/materialize.min.css"  media="screen,projection"/>
+
+	  	<!--Let browser know website is optimized for mobile-->
+	  	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
+	</head>
+	<body>
+		  <div class="navbar-fixed">
+			<nav>
+				<div class="nav-wrapper">
+      				<a href="#" class="brand-logo">Logo</a>
+					<ul class="right hide-on-med-and-down">
+						<li><a href="#">Menu1</a></li>
+						<li><a href="#">Menu2</a></li>
+						<li class="active"><a href="#">Menu3</a></li>
+					</ul>
+				</div>
+			</nav>
+		</div>
